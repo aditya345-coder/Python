01038680fac7e54c11b7c1260c6713dd8838189c
Aditya Gaharwar <92220550+aditya345-coder@users.noreply.github.com>
2026-07-01 00:27:03 +0530

    Create 584_FindCustomerReferee.py

---
+++ Pandas/Easy/584_FindCustomerReferee.py
@@ -0,0 +1,29 @@
+# Link: https://leetcode.com/problems/find-customer-referee/?envType=study-plan-v2&envId=top-sql-50
+
+"""Problem Description:
+Table: Customer
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| referee_id  | int     |
++-------------+---------+
+In SQL, id is the primary key column for this table.
+Each row of this table indicates the id of a customer, their name, and the id of the customer who referred them.
+ 
+
+Find the names of the customer that are either:
+
+referred by any customer with id != 2.
+not referred by any customer.
+Return the result table in any order.
+"""
+
+
+## Solution:
+import pandas as pd
+
+def find_customer_referee(customer: pd.DataFrame) -> pd.DataFrame:
+    return customer[(customer['referee_id']!=2) | (customer['referee_id'].isna())][['name']]
